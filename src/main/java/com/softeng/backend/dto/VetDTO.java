1dbf663d5b8973e572a771934f1eff2f0de76e62
Minh Phan <minhsg.phan@gmail.com>
2025-10-15 04:14:54 -0500

    chore(refactor): Add default value to DTO

--- src/main/java/com/softeng/backend/dto/VetDTO.java
+++ src/main/java/com/softeng/backend/dto/VetDTO.java
@@ -1,22 +1,27 @@
 package com.softeng.backend.dto;
 
 import com.softeng.backend.models.user.vet.Vet;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 import java.util.Map;
+import java.util.Objects;
 
-@AllArgsConstructor
-@NoArgsConstructor
 @Getter
 public class VetDTO {
-    private String id = "";
-    private Vet vet;
+    private final String id;
+    private final Vet vet;
+
+    public VetDTO(String id, Vet vet) {
+        this.id = Objects.requireNonNullElse(id, "");
+        this.vet = Objects.requireNonNullElse(vet, new Vet());
+    }
+
+    public VetDTO() {
+        this.id = "";
+        this.vet = new Vet();
+    }
 
     public Map<String, Object> toMap() {
-        if(vet == null)
-            vet = new Vet();
         return Map.of(
                 "id", id,
                 "firstName", vet.getFirstName(),
@@ -25,5 +30,9 @@ public class VetDTO {
                 "certification", vet.getCertification(),
                 "token", "MockTokenForNow"
         );
+    }
+
+    public boolean isEmpty() {
+        return id.isBlank() || vet.checkEmptyUser();
     }
 }
