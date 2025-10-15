9395358a56bff388f692477e62d0054eff4dd2b0
Victoria Iskandar <joyrunajacob@gmail.com>
2025-10-14 21:02:05 -0500

    trying to suppress logs

--- src/test/java/com/softeng/backend/BackendApplicationTests.java
+++ src/test/java/com/softeng/backend/BackendApplicationTests.java
@@ -14,14 +14,6 @@ class BackendApplicationTests {
     @MockitoBean
     private Firestore firestore;
 
-    // The following BeforeAll was copied from ChatGPT
-    @BeforeAll
-    static void setUpLogging() {
-        ch.qos.logback.classic.Logger root =
-                (ch.qos.logback.classic.Logger) LoggerFactory.getLogger(org.slf4j.Logger.ROOT_LOGGER_NAME);
-        root.setLevel(ch.qos.logback.classic.Level.INFO);
-    }
-
     @Test
     void contextLoads() {
     }
