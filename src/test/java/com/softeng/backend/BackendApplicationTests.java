e122f34c0f3687a612285e655d9eb88c7a37638f
Victoria Iskandar <joyrunajacob@gmail.com>
2025-10-14 20:43:40 -0500

    update CI to be less verbose maybe

--- src/test/java/com/softeng/backend/BackendApplicationTests.java
+++ src/test/java/com/softeng/backend/BackendApplicationTests.java
@@ -1,7 +1,9 @@
 package com.softeng.backend;
 
 import com.google.cloud.firestore.Firestore;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
@@ -11,6 +13,15 @@ class BackendApplicationTests {
 
     @MockitoBean
     private Firestore firestore;
+
+    // The following BeforeAll was copied from ChatGPT
+    @BeforeAll
+    static void setUpLogging() {
+        ch.qos.logback.classic.Logger root =
+                (ch.qos.logback.classic.Logger) LoggerFactory.getLogger(org.slf4j.Logger.ROOT_LOGGER_NAME);
+        root.setLevel(ch.qos.logback.classic.Level.INFO);
+    }
+
     @Test
     void contextLoads() {
     }
