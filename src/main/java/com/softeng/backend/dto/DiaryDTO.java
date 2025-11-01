b4e515ba88eeb1595c0d90d4e766539728e24617
Minh Phan <94029668+MinhPhan23@users.noreply.github.com>
2025-11-01 14:52:30 -0500

    feature(pet diary): Create model and DTO for pet diary (#18)
    * feature(pet diary): Create model and DTO for pet diary
    
    * feature(pet diary): Add attached file field

---
+++ src/main/java/com/softeng/backend/dto/DiaryDTO.java
@@ -0,0 +1,33 @@
+package com.softeng.backend.dto;
+
+import com.softeng.backend.models.diary.Diary;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.Map;
+
+@AllArgsConstructor
+@Getter
+public class DiaryDTO {
+    @NotNull @NotBlank
+    private final String id;
+    @NotNull @NotBlank
+    private final Diary diary;
+
+    public DiaryDTO() {
+        id = "";
+        diary = new Diary();
+    }
+
+    public Map<String, Object> toMap() {
+        return Map.of(
+                "id", id,
+                "contentType", diary.getContentType(),
+                "contentBody", diary.getContentType(),
+                "file", diary.getFiles(),
+                "createTimestamp", diary.getCreateTimestamp()
+        );
+    }
+}
