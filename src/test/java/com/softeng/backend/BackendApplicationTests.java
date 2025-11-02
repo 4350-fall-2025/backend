4c74ce235fd0ce26cbcf5b34703d523219ed2999
Minh Phan <minhsg.phan@gmail.com>
2025-11-02 01:12:40 -0600

    feature(firebase integration test): activate profile for BackendApplicationTests

--- src/test/java/com/softeng/backend/BackendApplicationTests.java
+++ src/test/java/com/softeng/backend/BackendApplicationTests.java
@@ -2,9 +2,11 @@ package com.softeng.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 
 @SpringBootTest
+@ActiveProfiles("firebase")
 class BackendApplicationTests {
 
     @Test
