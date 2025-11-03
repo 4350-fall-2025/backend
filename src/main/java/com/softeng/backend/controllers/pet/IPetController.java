c87c3b55061d60fc3ec10d746dd711e6c272b5f3
Minh Phan <minhsg.phan@gmail.com>
2025-11-03 12:38:58 -0600

    feature(create diary entry): add valid annotation

--- src/main/java/com/softeng/backend/controllers/pet/IPetController.java
+++ src/main/java/com/softeng/backend/controllers/pet/IPetController.java
@@ -2,6 +2,7 @@ package com.softeng.backend.controllers.pet;
 
 import com.softeng.backend.models.diary.Diary;
 import com.softeng.backend.models.pet.Pet;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import org.springframework.http.ResponseEntity;
@@ -24,5 +25,5 @@ public interface IPetController {
     ResponseEntity<Map<String, Object>> removePet(@NotNull @NotBlank @PathVariable String petId);
 
     //CREATE PET DIARY ENTRY
-    ResponseEntity<Map<String, Object>> addDiaryEntry(@NotNull @NotBlank @PathVariable String petId, @NotNull @RequestBody Diary diary);
+    ResponseEntity<Map<String, Object>> addDiaryEntry(@NotNull @NotBlank @PathVariable String petId, @NotNull @Valid @RequestBody Diary diary);
 }
