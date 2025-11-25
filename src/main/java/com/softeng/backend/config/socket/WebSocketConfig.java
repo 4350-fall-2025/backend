7319c781b601c67bae05e53b9b1d5d5b0be63afe
Minh Phan <minhsg.phan@gmail.com>
2025-11-25 15:38:15 -0600

    feature(vet online): cited Copilot

--- src/main/java/com/softeng/backend/config/socket/WebSocketConfig.java
+++ src/main/java/com/softeng/backend/config/socket/WebSocketConfig.java
@@ -6,6 +6,7 @@ import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBr
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
+//websocket configuration code from spring documentation
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
