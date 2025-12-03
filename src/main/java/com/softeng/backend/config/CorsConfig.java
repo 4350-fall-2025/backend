007c3f62b185577f83c580a241ad3a301402cad6
basmaabdulrazaq <abdulrab@myumanitoba.ca>
2025-12-03 14:34:30 -0600

    resolving sonarQube security issue

--- src/main/java/com/softeng/backend/config/CorsConfig.java
+++ src/main/java/com/softeng/backend/config/CorsConfig.java
@@ -18,7 +18,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOriginPatterns("*")
+                        .allowedOriginPatterns("http://localhost:3001")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
             }
